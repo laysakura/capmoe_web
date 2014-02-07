7f2789533ffb187d7955524bc34269cba63af996
Sho Nakatani <lay.sakura@gmail.com>
2014-02-07 14:05:45 +0900

    works wity Python 3.3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires = [
         # 'gunicorn',  # [todo] - introduce gunicorn as app server if too much load
         'Django',
-        'mysql-python',
+        'PyMySQL',
         # 'rainbow_logging_handler',
     ],
     tests_require = [
