9e304917177233ca1cd389db4df964cd72700c81
Sho Nakatani <lay.sakura@gmail.com>
2014-02-08 13:21:15 +0900

    adds capmoe_app

---
+++ capmoe_app/models.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+    capmoe_app.models
+    ~~~~~~~~~~~~~~~~~
+
+    :synopsis: Models for CapMoe web service
+
+    Description.
+"""
+
+
+# standard modules
+
+# 3rd party modules
+from django.db import models
+
+# original modules
