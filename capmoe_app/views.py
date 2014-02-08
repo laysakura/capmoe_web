d0bde4c4cfbc0fd284ba49c3d0797680730dd1a9
Sho Nakatani <lay.sakura@gmail.com>
2014-02-08 16:09:37 +0900

    adds /upload page template

--- capmoe_app/views.py
+++ capmoe_app/views.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+    capmoe_app.views
+    ~~~~~~~~~~~~~~~~
+
+    :synopsis: Describes what data to show
+
+    See: https://docs.djangoproject.com/en/dev/faq/general/#django-appears-to-be-a-mvc-framework-but-you-call-the-controller-the-view-and-the-view-the-template-how-come-you-don-t-use-the-standard-names
+"""
+
+
+# standard modules
+
+# 3rd party modules
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+# original modules
+
+
+def upload(request):
+    return HttpResponse("It works!!")
--- capmoe_web/urls.py
+++ capmoe_web/urls.py
@@ -3,10 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'capmoe_web.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = patterns(
+    '',
 
+    url(r'^', include('capmoe_app.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
