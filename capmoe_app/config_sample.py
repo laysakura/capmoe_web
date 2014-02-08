2d3dd319c91c9a78e4f07d0ba096e86978ab7ab9
Sho Nakatani <lay.sakura@gmail.com>
2014-02-08 16:18:35 +0900

    copy config_sample.py when travis ci

---
+++ capmoe_app/config_sample.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+    capmoe_app.config_sample
+    ~~~~~~~~~~~~~~~~~~~~~~~~
+
+    :synopsis: Sample of `config.py`
+
+    Rename this file into `config.py`
+"""
+
+
+from capmoe_app.config_default import config
+
+
+# [todo] - more informative sample?
+
+# config['imgdir'] = '/home/youruser/capmoe_img'
